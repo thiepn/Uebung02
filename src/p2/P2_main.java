5407b4509a9194e815d67516977563e5b4404853
thiepn <junsosmart@gmail.com>
2025-10-29 13:08:27 +0100

    uebung2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int i = 18;
+        boolean b = (i>10);
+        System.out.println(" i hat den Wert: " + i + ", b hat den Wert: " + b);
+
+    }
+}
