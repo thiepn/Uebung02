de920dc4bbe19796311e8fd055c6eb60ac32694c
thiepn <junsosmart@gmail.com>
2025-10-29 13:42:19 +0100

    hausaufgabe 2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,30 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        int i = 50;
+        int j = 12;
+        int k = 7;
+        int min;
+        int max;
+
+        if (i < j && i < k) {
+            min = i;
+        } else if (j < i && j < k) {
+            min = j;
+        } else {
+            min = k;
+        }
+
+        if (i > j && i > k) {
+            max = i;
+        } else if (j > i && j > k) {
+            max = j;
+        } else {
+            max = k;
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
---
+++ src/h3/H3_main.java
@@ -0,0 +1,20 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 1000;
+        int k = -10;
+
+        if (i>j && i<=200 && j<=100){
+            k = 1;
+        }else if (i>j && i>200 && j<=100){
+            k = 2;
+        }else if (i>j && i>200 && j>100){
+            k = 3;
+        }else if (i<j && i<=200 && j<=100){
+            k = 4;
+        }
+        System.out.println(k);
+    }
+}
